23ddabe99b99b06ba8bbcd3184da9075e116625c
Chrisotpher Luey <christopheraluey@gmail.com>
2021-10-14 13:40:20 -0700

    1.0.0
    Onboarding

---
+++ app/src/main/java/livechat/christopher/martin/onboard/Onboard.java
@@ -0,0 +1,78 @@
+package livechat.christopher.martin.onboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+import livechat.christopher.martin.R;
+
+public class Onboard extends AppCompatActivity {
+    private ViewPagerAdapter adapter;
+    private TextView[] dots;
+    private int[] layouts;
+    private ViewPager2 viewpager2;
+    private WormDotsIndicator wdi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_onboard);
+
+        layouts = new int[]{R.layout.onboard_fragment, R.layout.onboard_fragment};
+
+        wdi = findViewById(R.id.onboard_worm_dots);
+        viewpager2 = findViewById(R.id.onboard_viewpager);
+        adapter = new ViewPagerAdapter();
+        viewpager2.setAdapter(adapter);
+        wdi.setViewPager2(viewpager2);
+    }
+
+    public class ViewPagerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        public ViewPagerAdapter() {
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+            return new SliderViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return layouts[position];
+        }
+
+        @Override
+        public int getItemCount() {
+            return layouts.length;
+        }
+
+        public class SliderViewHolder extends RecyclerView.ViewHolder {
+            public TextView title, year, genre;
+
+            public SliderViewHolder(View view) {
+                super(view);
+            }
+        }
+    }
+}
